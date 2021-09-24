b7e1663a462db4c71a1c9782324d8779a1ac2859
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-09-24 22:46:25 +1000

    knn_5,7,nb,lr

--- TwitterData.py
+++ TwitterData.py
@@ -3,13 +3,35 @@ import math
 import ast
 from random import random
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 
 class TwitterData:
     def __init__(self):
         self.train_labels, self.train_tweet_ids, self.train_data = self.read_data_glove("data/train_glove.csv")
         self.test_labels, self.test_tweet_ids, self.test_data = self.read_data_glove("data/test_glove.csv")
-        predictions = self.knn_preds(2, self.train_data, self.train_labels, self.test_data)
-        self.write_predictions(predictions, "predictions/knn_2_glove_preds.csv")
+        # lr
+        predictions = self.lr_preds(self.train_data, self.train_labels, self.test_data)
+        self.write_predictions(predictions, "predictions/lr_glove_preds.csv")
+
+        # nb
+        # predictions = self.nb_preds(self.train_data, self.train_labels, self.test_data)
+        # self.write_predictions(predictions, "predictions/nb_glove_preds.csv")
+
+        # knn
+        # predictions = self.knn_preds(7, self.train_data, self.train_labels, self.test_data)
+        # self.write_predictions(predictions, "predictions/knn_7_glove_preds.csv")
+
+    def lr_preds(self, train_data, train_labels, test_data):
+        lr = LogisticRegression().fit(train_data, train_labels)
+        predictions = lr.predict(test_data)
+        return predictions
+
+    def nb_preds(self, train_data, train_labels, test_data):
+        gnb = GaussianNB()
+        gnb.fit(train_data, train_labels)
+        predictions = gnb.predict(test_data)
+        return predictions
 
     def knn_preds(self, neighbours, train_data, train_labels, test_data):
         print("knn")
